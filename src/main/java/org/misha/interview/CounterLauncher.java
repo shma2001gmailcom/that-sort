8271351e5463f6b5dbf76737910737cf996dd939
misha <shma2001@gmail.com>
2016-03-16 11:41:28 +0600

    create launcher

---
+++ src/main/java/org/misha/interview/CounterLauncher.java
@@ -0,0 +1,9 @@
+package org.misha.interview;
+
+/**
+ * author: misha
+ * date: 3/16/16
+ * time: 11:13 AM
+ */
+public class CounterLauncher {
+}
---
+++ src/main/java/org/misha/interview/Worker.java
@@ -0,0 +1,9 @@
+package org.misha.interview;
+
+/**
+ * author: misha
+ * date: 3/16/16
+ * time: 11:30 AM
+ */
+public class Worker {
+}
