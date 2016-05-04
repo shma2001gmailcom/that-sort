49e10eddf7a98719ca0b11992551dab2d2f4f123
sbt-shevelin-ma <mashevelin.sbt@sberbank.ru>
2016-05-04 12:09:50 +0600

    change run.sh for work

--- bash/run.sh
+++ bash/run.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+javahome="C:/work/Java/jdk1.6.0_45"
+M2_HOME='D:/work/maven/'
+export M2_HOME
+M2=${M2_HOME}/bin
+export M2
+mvn='D:/work/maven/bin/mvn.bat'
+export JAVA_HOME=${javahome}
 cd ../
-mvn clean install
+${mvn} clean install
 cd target
 java -jar that-sort.jar
