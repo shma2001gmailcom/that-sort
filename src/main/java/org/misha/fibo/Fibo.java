7ec7154154a8d202bd5af2da8b3cd081db31e947
misha <shma2001@gmail.com>
2016-10-28 21:54:57 +0600

    calculate fibo(3000)

--- src/main/java/org/misha/fibo/Fibo.java
+++ src/main/java/org/misha/fibo/Fibo.java
@@ -1,5 +1,7 @@
 package org.misha.fibo;
 
+import java.math.BigInteger;
+
 /**
  * author: misha
  * date: 10/27/16
@@ -13,7 +15,15 @@ class Fibo {
         return y;
     }
 
-    int fibo(int n, int a, int b) {
+    long fibo(long n, long a, long b) {
         return n < 1 ? 0 : n < 2 ? 1 : n == 2 ? b : fibo(n - 1, b, a + b);
+    }
+
+    BigInteger fibonacci(int n, BigInteger a, BigInteger b) {
+        return n < 1 ? bigInt(0) : n < 2 ? bigInt(1) : n == 2 ? b : fibonacci(n - 1, b, a.add(b));
+    }
+
+    static BigInteger bigInt(long val) {
+        return new BigInteger(String.valueOf(val));
     }
 }
