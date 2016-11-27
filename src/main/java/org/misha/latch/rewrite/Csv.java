4908478be364aca69ce1eb2023417000652c3926
misha <shma2001@gmail.com>
2016-11-27 18:09:38 +0600

    reformat code

--- src/main/java/org/misha/latch/rewrite/Csv.java
+++ src/main/java/org/misha/latch/rewrite/Csv.java
@@ -3,11 +3,14 @@ package org.misha.latch.rewrite;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.CountDownLatch;
+
+import static java.text.MessageFormat.format;
 
 /**
  * Author: mshevelin
@@ -17,13 +20,12 @@ import java.util.concurrent.CountDownLatch;
  * class parses csv file, makes db calls, retrieves results and writes it to log file
  */
 public final class Csv {
-    private static final Logger log = Logger.getLogger(Csv.class);
-    private static final int batchLimit = 10;
+    static final Logger log = Logger.getLogger(Csv.class);
     private static final int totalSlots = 200;
     private final String filePath;
     private volatile int rowCount;
 
-    public Csv(final String filePath) {
+    private Csv(final String filePath) {
         this.filePath = filePath;
     }
 
@@ -37,7 +39,7 @@ public final class Csv {
         csv.convert();
     }
 
-    public void convert() {
+    private void convert() {
         doRows();
     }
 
@@ -82,186 +84,13 @@ public final class Csv {
         try {
             sc = new Scanner(new File(filePath));
         } catch (final FileNotFoundException e) {
-            log.error(e.getMessage() + " necessary file: " + filePath + " is not found, program is shutting down."
+            log.error(format("{0} necessary file: {1} is not found, program is shutting down.",
+                             e.getMessage(),
+                             filePath)
             );
             throw new RuntimeException(e);
         }
         return sc;
-    }
-
-    private static class Slots implements Runnable {
-        private final int slotCount;
-        private final CountDownLatch waitForInsert;
-        private final int rowCount;
-        private final List<String> cache;
-
-        private Slots(
-                final int slotCount, final CountDownLatch latch, final int rowCount, final List<String> cache
-        ) {
-            this.slotCount = slotCount;
-            waitForInsert = latch;
-            this.rowCount = rowCount;
-            this.cache = cache;
-        }
-
-        private static Slots createSlots(
-                final int slotCount, final CountDownLatch latch1, final int rowCount, final List<String> cache
-        ) {
-            return new Slots(slotCount, latch1, rowCount, cache);
-        }
-
-        private void convertSlots() {
-            assert rowCount >= slotCount : "row count should be not less than slot count.";
-            final int rowsPerSlot = rowCount / slotCount;
-            final int residue = rowCount % slotCount;
-            final CountDownLatch latch = new CountDownLatch((residue == 0) ? slotCount : (1 + slotCount));
-            final Thread logThread = new Thread(LogWriter.createLogWriter(latch));
-            logThread.start();
-            for (int i = 0; i < slotCount; ++i) {
-                final Thread oneSlotThread = new Thread(Slot.createSlot(cache, i, rowsPerSlot, latch), "doSlot#" + i);
-                oneSlotThread.start();
-            }
-            if (residue > 0) {
-                final Thread residueSlotThread =
-                        new Thread(Slot.createSlot(cache, 1 + slotCount, residue, latch), "doSlot#" + (1 + slotCount));
-                residueSlotThread.start();
-            }
-        }
-
-        @Override
-        public void run() {
-            try {
-                waitForInsert.await();
-                convertSlots();
-            } catch (final InterruptedException ignored) {
-                Thread.currentThread().interrupt();
-            }
-        }
-    }
-
-    private static class Slot implements Runnable {
-        private final CountDownLatch latch;
-        private final List<String> slotCache = new ArrayList<String>();
-        private final List<String> cache;
-        private final int size;
-        private final int from;
-        private final int to;
-
-        private Slot(
-                final List<String> cache, final int slotNumber, final int rowsPerSlot, final CountDownLatch latch
-        ) {
-            this.latch = latch;
-            this.cache = cache;
-            size = cache.size();
-            from = ((slotNumber * rowsPerSlot) <= size) ? (slotNumber * rowsPerSlot)
-                                                        : (slotNumber * (size / slotNumber));
-            to = ((from + rowsPerSlot) <= size) ? (from + rowsPerSlot) : size;
-        }
-
-        private static Slot createSlot(
-                final List<String> cache, final int slotNumber, final int rowsPerSlot, final CountDownLatch latch
-        ) {
-            final Slot result = new Slot(cache, slotNumber, rowsPerSlot, latch);
-            result.addRowsToSlot();
-            return result;
-        }
-
-        private void addRowsToSlot() {
-            slotCache.addAll(cache.subList(from, to));
-        }
-
-        private void doSlot() {
-            final File file = new File("./answer/" + from + "-" + to);
-            Writer writer = null;
-            try {
-                writer = new FileWriter(file, false);
-                for (final String record : slotCache) {
-                    writer.write(record + "\n");
-                    writer.flush();
-                }
-            } catch (final IOException ignored) {
-                //e.printStackTrace();
-            } finally {
-                if (writer != null) {
-                    try {
-                        writer.close();
-                    } catch (IOException e) {
-                        log.error(e);
-                    }
-                }
-            }
-        }
-
-        @Override
-        public void run() {
-            doSlot();
-            latch.countDown();
-        }
-    }
-
-    private static class LogWriter implements Runnable {
-        private final CountDownLatch latch;
-
-        private LogWriter(final CountDownLatch latch) {
-            this.latch = latch;
-        }
-
-        public static LogWriter createLogWriter(final CountDownLatch latch) {
-            return new LogWriter(latch);
-        }
-
-        private static void writeLogs() {
-            //make something meaning here
-            log.error("writing logs");
-        }
-
-        @Override
-        public void run() {
-            try {
-                latch.await();
-            } catch (final InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-            log.info("latch is open");
-            writeLogs();
-        }
-    }
-
-    private static class BulkInsert implements Runnable {
-        private final List<String> cache;
-        private final CountDownLatch latch;
-
-        private BulkInsert(
-                final List<String> cache, final CountDownLatch latch
-        ) {
-            this.cache = cache;
-            this.latch = latch;
-        }
-
-        private static BulkInsert createBulkInsert(
-                final List<String> cache, final CountDownLatch latch
-        ) {
-            return new BulkInsert(cache, latch);
-        }
-
-        private void bulkInsert() {
-            int counter = 0;
-            for (final String row : cache) {
-                ++counter;
-                if ((counter % batchLimit) == 0) {
-                    //make something meaning here
-                    log.debug(counter + "--execute batch" + row);
-                }
-            }
-            //make something meaning here
-            log.trace(counter + "--execute batch reminder");
-        }
-
-        @Override
-        public void run() {
-            bulkInsert();
-            latch.countDown();
-        }
     }
 }
 
