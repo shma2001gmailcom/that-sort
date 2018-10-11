2d99eee0de9f8980663411a92ea9d60fe8bb48b5
shma2001gmailcom <shma2001@gmail.com>
2018-10-11 13:44:06 +0300

    change build.sh

--- bash/run.sh
+++ bash/run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-javahome="C:/work/Java/jdk1.6.0_45"
-M2_HOME='D:/work/maven/'
+javahome='C:\Program Files\Java\jdk1.8.0_171'
+M2_HOME='C:\maven-3.5.4'
 export M2_HOME
-M2=${M2_HOME}/bin
+M2=${M2_HOME}\\bin
 export M2
-mvn='D:/work/maven/bin/mvn.bat'
+mvn=${M2}\\mvn.cmd
 export JAVA_HOME=${javahome}
 cd ../
 ${mvn} clean install
