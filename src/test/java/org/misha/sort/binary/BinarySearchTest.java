909722a60dbd0e818b43c81efb13291c5f987259
misha <shma2001@gmail.com>
2019-09-08 15:33:44 +0300

    add test

--- src/test/java/org/misha/sort/binary/BinarySearchTest.java
+++ src/test/java/org/misha/sort/binary/BinarySearchTest.java
@@ -1,19 +1,47 @@
 package org.misha.sort.binary;
 
 import com.google.common.collect.Lists;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
+import javax.annotation.Nullable;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
-import static org.junit.Assert.*;
+import static java.util.stream.Collectors.toList;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class BinarySearchTest {
+
     @Test
     public void testSearch() throws Exception {
         List<Integer> data = Lists.newArrayList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
         assertTrue(data.stream()
-                      .map(i -> new BinarySearch<>(data).search(i) == i)
-                      .reduce((x, y) -> x && y)
-                      .orElse(false));
+                       .map(i -> new BinarySearch<>(data).search(i) == i)
+                       .reduce((x, y) -> x && y)
+                       .orElse(false));
+    }
+
+    @Test
+    public void testSearchObjects() {
+        List<O> list = Lists.newArrayList(null, new O(), new O(), new O(), null, null);
+        List<O> cleared = list.stream().filter(Objects::nonNull).collect(toList());
+        cleared.sort(Comparator.comparingInt(Object::hashCode));
+        cleared.forEach(o -> {
+                int expected = new BinarySearch<>(cleared).search(o);
+                int actual = cleared.indexOf(o);
+                if (expected != actual) {
+                    fail(expected + " != " + actual + " for o = " + o);
+                }
+            });
+    }
+
+    static class O implements Comparable<O> {
+        @Override
+        public int compareTo(@Nullable final O o) {
+            return o == null ? 1 : hashCode() - o.hashCode();
+        }
     }
 }
