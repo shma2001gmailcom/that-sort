545adc6dd4455a4e4c62a5664ab0b46687db2db9
misha <shma2001@gmail.com>
2022-05-11 00:41:48 +0300

    let code with tin
    expand nested lists

---
+++ src/main/java/org/misha/luxoft/MultiListTest.java
@@ -0,0 +1,52 @@
+package org.misha.luxoft;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.misha.interview.luxoft.MultiList.withChildren;
+import static org.misha.interview.luxoft.MultiList.withData;
+
+public class MultiListTest {
+
+    @Test
+    public void test() {
+        testCommon();
+        testSingleton();
+        testEmpty();
+    }
+
+    private void testEmpty() {
+        assertEquals(new ArrayList<>(), withChildren().asList());
+    }
+
+    private void testSingleton() {
+        assertEquals(new ArrayList<Integer>() {{add(1);}}, withData(1).asList());
+    }
+
+    private void testCommon() {
+        MultiList x =
+                withChildren(
+                        withChildren(
+                                withData(1),
+                                withData(6),
+                                withChildren(
+                                        withData(3),
+                                        withData(4),
+                                        withData(5)
+                                )
+                        ),
+                        withData(2)
+                );
+        ArrayList<Integer> expected = new ArrayList<Integer>() {{
+            add(1);
+            add(6);
+            add(3);
+            add(4);
+            add(5);
+            add(2);
+        }};
+        assertEquals(expected, x.asList());
+    }
+}
