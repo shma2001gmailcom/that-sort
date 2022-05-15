3e0ff1ccef2d7024bbe03373151928b9a5c66134
misha <shma2001@gmail.com>
2022-05-15 15:39:26 +0300

    let code with tin
    merge test

---
+++ src/main/java/org/misha/luxoft/OverlappingTest.java
@@ -0,0 +1,20 @@
+package org.misha.luxoft;
+
+import org.junit.Test;
+
+import static org.misha.interview.Overlapping.range;
+
+public class OverlappingTest {
+    @Test
+    public void test() {
+        Overlapping o = new Overlapping(range(0, 6),
+                                        range(3, 8),
+                                        range(7, 9),
+                                        range(10, 13),
+                                        range(15, 18),
+                                        range(15, 19),
+                                        range(20, 55));
+        System.out.println(o);
+        System.out.println(o.merge());
+    }
+}
